c1012dea9928351be1e4ec078d58961ca635a36c
Admin <Admin@NITESHKUMARJANG>
2023-07-11 16:09:13 +0530

    11-07

--- App.js
+++ App.js
@@ -6,19 +6,29 @@ const element = React.createElement(
     'h1',
     { id: 'heading' },
     'Heading 1'
-    );
-    
-    const element2 = React.createElement(
+);
+
+const element2 = React.createElement(
     'h1',
     { className: 'heading' },
     'Heading 2'
-    );
-    
-    const mydv = React.createElement(
-    'div',
-    { className: 'dvTest' },
-    [element,element2]
-    );
-    
-    const root = ReactDOM.createRoot(document.getElementById('root'));
-    root.render(mydv);
+);
+
+// const mydv = React.createElement(
+// 'div',
+// { className: 'dvTest' },
+// [element,element2]
+// );
+
+const Mydv = () => {
+    return (
+        <React.Fragment>
+            <div>Div 1</div>
+            <div>Div 2</div>
+            <div>Div 3</div>
+        </React.Fragment>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Mydv />);
